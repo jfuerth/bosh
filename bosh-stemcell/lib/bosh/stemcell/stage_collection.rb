cff625bb4410e9100ffc4195f9f9797180de94fa
Jonathan Fuerth and Nader Ziada <pair+jfuerth+nziada@pivotal.io>
2015-04-01 14:11:54 -0400

    Reduce daily cron load and randomize running of periodic jobs
    Tracker story: https://www.pivotaltracker.com/story/show/88733114

--- bosh-stemcell/lib/bosh/stemcell/stage_collection.rb
+++ bosh-stemcell/lib/bosh/stemcell/stage_collection.rb
@@ -101,6 +101,7 @@ module Bosh::Stemcell
           :rsyslog_config,
           :delay_monit_start,
           :system_grub,
+          :cron_config,
       ].flatten
 
       if operating_system.version.to_f < 7
@@ -120,6 +121,7 @@ module Bosh::Stemcell
         :delay_monit_start,
         :system_grub,
         :rhel_unsubscribe,
+        :cron_config,
       ].flatten
     end
 
@@ -140,6 +142,7 @@ module Bosh::Stemcell
         :delay_monit_start,
         :system_grub,
         :vim_tiny,
+        :cron_config,
       ].flatten
     end
 
